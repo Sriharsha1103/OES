ec162887f715d326113d9465bd7318feed476cd4
Admin <ngsgcrackit@gmail.com>
2020-02-18 15:33:49 +0530

    Initial commit

---
+++ app/src/main/java/com/admin/oes/Handler.java
@@ -0,0 +1,16 @@
+package com.admin.oes;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class Handler extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+    }
+}
